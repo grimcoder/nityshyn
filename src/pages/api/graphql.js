112592ecb35976bc26e6ca0c32063ce3ade52ef1
Taras KOvtun <taraskovtun@gmail.com>
2023-10-20 09:51:54 -0700

    added gql schema

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -16,14 +16,34 @@ function computeSecretHash(clientId, secretKey, username) {
     return hmac.digest('base64');
 }
 
-
-
 const typeDefs = gql`
 
 type Query {
-    _empty: String
+    # Tenants
+    tenant(id: ID!): Tenant
+    tenants: [Tenant!]
+
+    # Users
+    user(id: ID!): User
+    users: [User!]
+
+    # Articles
+    article(id: ID!): Article
+    articles: [Article!]
+
+    # Media
+    media(id: ID!): Media
+    medias: [Media!]
+
+    # Comments
+    comment(id: ID!): Comment
+    comments: [Comment!]
+
+    # Categories
+    category(id: ID!): Category
+    categories: [Category!]
   }
-  
+
   type Mutation {
       register(input: RegisterInput!): RegisterResponse!
   }
@@ -40,27 +60,221 @@ type Query {
       user: User
   }
 
-  type User {
-      email: String!
-  }
-
   type Mutation {
     login(input: LoginInput!): LoginResponse!
+
+    createTenant(input: CreateTenantInput!): Tenant!
+    updateTenant(id: ID!, input: UpdateTenantInput!): Tenant!
+    deleteTenant(id: ID!): DeleteResponse!
+
+    # Users
+    createUser(input: CreateUserInput!): User!
+    updateUser(id: ID!, input: UpdateUserInput!): User!
+    deleteUser(id: ID!): DeleteResponse!
+
+    # Articles
+    createArticle(input: CreateArticleInput!): Article!
+    updateArticle(id: ID!, input: UpdateArticleInput!): Article!
+    deleteArticle(id: ID!): DeleteResponse!
+
+    # Media
+    createMedia(input: CreateMediaInput!): Media!
+    updateMedia(id: ID!, input: UpdateMediaInput!): Media!
+    deleteMedia(id: ID!): DeleteResponse!
+
+    # Comments
+    createComment(input: CreateCommentInput!): Comment!
+    updateComment(id: ID!, input: UpdateCommentInput!): Comment!
+    deleteComment(id: ID!): DeleteResponse!
+
+    # Categories
+    createCategory(input: CreateCategoryInput!): Category!
+    updateCategory(id: ID!, input: UpdateCategoryInput!): Category!
+    deleteCategory(id: ID!): DeleteResponse!
+
+    # Article-Category Relationship
+    addArticleToCategory(articleId: ID!, categoryId: ID!): Article!
+    removeArticleFromCategory(articleId: ID!, categoryId: ID!): Article!
   }
-  
+
   input LoginInput {
     email: String!
     password: String!
   }
-  
+
+
+# Tenant Mutations
+input CreateTenantInput {
+    name: String!
+    domain: String!
+}
+
+input UpdateTenantInput {
+    name: String
+    domain: String
+}
+
+# User Mutations
+input CreateUserInput {
+    tenant_id: ID!
+    email: String!
+    password: String! # Ensure this is hashed before being saved to the database
+    role: UserRole!
+}
+
+input UpdateUserInput {
+    tenant_id: ID
+    email: String
+    password: String
+    role: UserRole
+}
+
+# Article Mutations
+input CreateArticleInput {
+    tenant_id: ID!
+    author_id: ID!
+    title: String!
+    content: String!
+    published_at: String
+}
+
+input UpdateArticleInput {
+    tenant_id: ID
+    author_id: ID
+    title: String
+    content: String
+    published_at: String
+}
+
+# Media Mutations
+input CreateMediaInput {
+    tenant_id: ID!
+    article_id: ID
+    url: String!
+    type: MediaType!
+}
+
+input UpdateMediaInput {
+    tenant_id: ID
+    article_id: ID
+    url: String
+    type: MediaType
+}
+
+# Comment Mutations
+input CreateCommentInput {
+    article_id: ID!
+    author_id: ID!
+    content: String!
+}
+
+input UpdateCommentInput {
+    article_id: ID
+    author_id: ID
+    content: String
+}
+
+# Category Mutations
+input CreateCategoryInput {
+    tenant_id: ID!
+    name: String!
+    description: String
+}
+
+input UpdateCategoryInput {
+    tenant_id: ID
+    name: String
+    description: String
+}
+
+# Standard Response for Delete Mutations
+type DeleteResponse {
+    success: Boolean!
+    message: String
+}
+
   type LoginResponse {
     success: Boolean!
     message: String!
     token: String
     user: User
   }
-  
-  
+
+  type Tenant {
+    tenant_id: ID!
+    name: String!
+    domain: String!
+    created_at: String!
+    updated_at: String!
+    users: [User!]
+    articles: [Article!]
+    media: [Media!]
+    categories: [Category!]
+}
+
+type User {
+    user_id: ID!
+    tenant: Tenant!
+    email: String!
+    password: String! # Be cautious about returning passwords!
+    role: UserRole!
+    created_at: String!
+    updated_at: String!
+    articles: [Article!]
+    comments: [Comment!]
+}
+
+enum UserRole {
+    admin
+    editor
+    viewer
+}
+
+type Article {
+    article_id: ID!
+    tenant: Tenant!
+    author: User!
+    title: String!
+    content: String!
+    published_at: String
+    created_at: String!
+    updated_at: String!
+    media: [Media!]
+    comments: [Comment!]
+    categories: [Category!]
+}
+
+type Media {
+    media_id: ID!
+    tenant: Tenant!
+    article: Article
+    url: String!
+    type: MediaType!
+    created_at: String!
+    updated_at: String!
+}
+
+enum MediaType {
+    image
+    video
+}
+
+type Comment {
+    comment_id: ID!
+    article: Article!
+    author: User!
+    content: String!
+    created_at: String!
+    updated_at: String!
+}
+
+type Category {
+    category_id: ID!
+    tenant: Tenant!
+    name: String!
+    description: String
+    articles: [Article!]
+}
 `;
 
 const ClientId = process.env.YOUR_COGNITO_APP_CLIENT_ID;
@@ -80,10 +294,10 @@ const resolvers = {
                     UserAttributes: [
                         {
                             Name: 'email',
-                            Value: input.email 
+                            Value: input.email
                         }
                     ],
-                    SecretHash  
+                    SecretHash
                 };
 
                 const response = await cognito.signUp(params).promise();
@@ -107,30 +321,30 @@ const resolvers = {
 
 
             const params = {
-              AuthFlow: 'USER_PASSWORD_AUTH',
-              ClientId,
-              AuthParameters: {
-                USERNAME: input.email,
-                PASSWORD: input.password,
-                SECRET_HASH
-              },
+                AuthFlow: 'USER_PASSWORD_AUTH',
+                ClientId,
+                AuthParameters: {
+                    USERNAME: input.email,
+                    PASSWORD: input.password,
+                    SECRET_HASH
+                },
             };
-      
+
             try {
-              const response = await cognito.initiateAuth(params).promise();
-              return {
-                success: true,
-                message: 'Login successful',
-                token: response.AuthenticationResult.IdToken, // or AccessToken, depending on your needs
-                user: { email: input.email }
-              };
+                const response = await cognito.initiateAuth(params).promise();
+                return {
+                    success: true,
+                    message: 'Login successful',
+                    token: response.AuthenticationResult.IdToken, // or AccessToken, depending on your needs
+                    user: { email: input.email }
+                };
             } catch (error) {
-              return {
-                success: false,
-                message: error.message
-              };
+                return {
+                    success: false,
+                    message: error.message
+                };
             }
-          }
+        }
     }
 };
 
@@ -146,6 +360,11 @@ export const config = {
 
 // Use Next.js API route handler to start the Apollo Server and create the handler
 export default async (req, res) => {
-  await apolloServer.start();
-  return apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+    try {
+        if (apolloServer.state.phase !== 'started') await apolloServer.start();
+        return apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+    }
+    catch (ex) {
+        const a = 0;
+    }
 };
